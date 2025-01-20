a9e3be4b62b9e579b4018598dec012515afd9740
Andrii9991 <andriymyshchyshyn6@gmail.com>
2025-01-20 10:12:58 +0200

    feat: create base structure for project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col items-center">
+    <input type="text" />
+
+    <div v-for="product in allProducts" :key="product.id">
+      {{ product.title }}
+    </div>
+
+    <button class="bg-red-500">Отримати данні</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { ref } from "vue";
+import { onMounted } from "vue";
+import { getAllProducts } from "./api/mainRequests";
+import type { IProduct } from "./interfaces/product";
+
+const allProducts = ref<IProduct[]>([]);
+
+onMounted(async () => {
+  allProducts.value = await getAllProducts();
+  allProducts.value;
+});
+</script>
+
+<style scoped></style>
